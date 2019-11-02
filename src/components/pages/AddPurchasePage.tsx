7a822c4334be9881fd3435a74a61d96bf307330a
Alex <sasha-elahov@yandex.ru>
2019-11-02 12:33:27 +0300

    init routing

---
+++ src/components/pages/AddPurchasePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddPurchasePage: React.FC = (props) => {
+	return (
+		<h1>AddPyrchasePage</h1>
+	)
+}
+
+export default AddPurchasePage;
---
+++ src/components/pages/SettingsPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SettingsPage: React.FC = (props) => {
+	return (
+		<h1>SettingsPage</h1>
+	)
+}
+
+export default SettingsPage;
