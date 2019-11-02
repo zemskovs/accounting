dd2ee62215c2aea85fadec5cde7c53b917fa7dc0
Vladimir Zemskov <vlzemskovs@gmail.com>
2019-11-02 15:20:24 +0300

    add fetch data

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -10,6 +10,7 @@ import {
 import BottomBar, { Pages } from "../bottomBar/BottomBar";
 import { Doughnut } from "react-chartjs-2";
 import CostList from "../costsList/CostsList";
+import { Cost } from "../../models/models";
 
 const data = {
 	labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
@@ -38,25 +39,71 @@ const data = {
 	]
 };
 //toDO: разобраться с иконками
-export default () => (
-	<Page>
-		<Navbar title="Бухгалтерия" />
-		<BottomBar tabIndex={Pages.home} />
-		<Swiper>
-			<SwiperSlide>
-				<Block>
-					<Doughnut
-						data={data}
-						options={{ legend: { display: false } }}
-					/>
-				</Block>
-				<BlockTitle>Расходы по категориям</BlockTitle>
-				<CostList
-					costs={[{ title: "one", icon: "inbox", total: 12 }]}
-				/>
-			</SwiperSlide>
-			<SwiperSlide>Slide 2</SwiperSlide>
-			<SwiperSlide>Slide 3</SwiperSlide>
-		</Swiper>
-	</Page>
-);
+export default () => {
+	const [cost, setCost] = React.useState<Cost[]>([
+		{ title: "", total: 0, icon: "" }
+	]);
+
+	React.useEffect(() => {
+		let active = true;
+		const abortCtrl = new AbortController();
+
+		fetch("https://zemskovs.github.io/accounting/src/demoData/cost.json", {
+			signal: abortCtrl.signal
+		})
+			.then(x => x.json())
+			.then(costs => {
+				active && setCost(costs.costs);
+			});
+
+		return () => {
+			active = false;
+			abortCtrl.abort();
+		};
+	}, []);
+
+	return (
+		<Page>
+			<Navbar title="Бухгалтерия" />
+			<BottomBar tabIndex={Pages.home} />
+			{cost && (
+				<Swiper>
+					<SwiperSlide>
+						<Block>
+							<Doughnut
+								data={{
+									labels: cost.map(x => x.title),
+									datasets: [{
+										data: cost.map(x => x.total),
+										backgroundColor: [
+											"rgba(255, 99, 132, 0.2)",
+											"rgba(54, 162, 235, 0.2)",
+											"rgba(255, 206, 86, 0.2)",
+											"rgba(75, 192, 192, 0.2)",
+											"rgba(153, 102, 255, 0.2)",
+											"rgba(255, 159, 64, 0.2)"
+										],
+										borderColor: [
+											"rgba(255, 99, 132, 1)",
+											"rgba(54, 162, 235, 1)",
+											"rgba(255, 206, 86, 1)",
+											"rgba(75, 192, 192, 1)",
+											"rgba(153, 102, 255, 1)",
+											"rgba(255, 159, 64, 1)"
+										],
+										borderWidth: 1
+									}]
+								}}
+								options={{ legend: { display: false } }}
+							/>
+						</Block>
+						<BlockTitle>Расходы по категориям</BlockTitle>
+						<CostList costs={cost} />
+					</SwiperSlide>
+					<SwiperSlide>Slide 2</SwiperSlide>
+					<SwiperSlide>Slide 3</SwiperSlide>
+				</Swiper>
+			)}
+		</Page>
+	);
+};
